30ab5fdbfde9c0d35a6ae7c172d8438462e3b625
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-08 18:05:51 +0000

    print [a..z][A..Z]

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case, then in uppercase
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
