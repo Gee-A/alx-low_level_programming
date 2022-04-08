8db34c0a4db86646217d5afb16eee0b2c6658b97
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-08 21:11:17 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single digit number
+ * number should be separated by ',' and printed in ascending order
+ *
+ * Return: 0 if successful, else non-zero
+ */
+int main(void)
+{
+	char ch = 48;
+
+	while (ch <= 57)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
