0b0394444171b89f38a251915c0cecbcb4b6ce51
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-14 04:31:56 +0000

    fibonacci's getting too long?, split it!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2, l;
 
 	j = 1;
 	k = 2;
@@ -20,15 +20,16 @@ int main(void)
 		j = k - j;
 	}
 
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
+	l = 1000000000;
+	j1 = j / l;
+	j2 = j % l;
+	k1 = k / l;
+	k2 = k % l;
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
+		printf(", %lu", k1 + (k2 / l));
+		printf("%lu", k2 % l);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
