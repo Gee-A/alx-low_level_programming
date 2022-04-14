beae5c89f0244b11706c2670e68b9bd252df8826
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-14 01:36:33 +0000

    Add two digit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add @a and @b
+ * @a: first parameter for the addition
+ * @b: second parameter for the addition
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
