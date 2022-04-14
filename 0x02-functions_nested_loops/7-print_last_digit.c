a9bb2c808919be468b70f84186ba8ffeb73a009a
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-14 00:15:35 +0000

    It's what we do with things that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of the input number
+ * @n: number
+ * Return: last digit of number @n
+ */
+int print_last_digit(int n)
+{
+	int l;
+	
+	l = n % 10;
+	return ((l >= 0) ? l : l * -1);
+}
