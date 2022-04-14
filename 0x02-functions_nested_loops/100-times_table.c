607453a953498a7236dbad97ab07b98ed3d41957
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-04-14 02:36:24 +0000

    multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - print times table of any entry less than 15
+ * @n: requested parameter
+ *
+ * Return: void, print times table.
+ */
+
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return;
+	int row, col, product, huns, tens, units;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			product = row * col;
+
+			if (col == 0)
+				_putchar('0');
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+			else if (product < 100)
+			{
+				_putchar(' ');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			if (col < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
