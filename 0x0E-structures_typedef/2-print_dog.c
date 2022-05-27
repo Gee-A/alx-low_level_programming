dfb19e3b3f3ef37ded5d8301f41fafed4f630c92
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-05-27 03:51:34 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
-		printf("Name: %S\n", d->name);
+		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
