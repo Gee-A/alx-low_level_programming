b7998948e5a52d172e820a360baf877cb272afb6
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-05-27 04:40:30 -0700

    commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints an integer
+ * @array: is a pointer to int
+ * @action: is a pointer to the function
+ * @size: is the size of the array
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
