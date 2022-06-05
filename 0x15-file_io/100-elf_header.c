27d7bd8d1dc5f47f0f0911345cc75d129d5b0a7d
Gideon Adebowale <gideonadebowale66@gmail.com>
2022-06-05 14:51:03 -0700

    displays the informatin contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,9 +1,9 @@
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "main.h"
 #include <unistd.h>
 
 void check_elf(unsigned char *e_ident);
